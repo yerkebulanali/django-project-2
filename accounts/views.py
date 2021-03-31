c9cabfc4925b1f73e1f1b93521675099d95282e6
Yerkebulan <yerkebulanali@gmail.com>
2021-03-31 13:02:17 +0600

    add: products, customer, home views

--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import *
 
 def home(request):
-    return render(request, 'accounts/dashboard.html')
+    orders = Orders.objects.all()
+    customers = Customer.objects.all()
+    total_customers = customers.count()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    context = {'orders':orders, 'customers': customers,
+               'total_customers': total_customers,
+               'total_orders': total_orders,
+               'delivered': delivered, 'pending': pending}
+    return render(request, 'accounts/dashboard.html', context)
 
 def products(request):
-    return render(request, 'accounts/products.html')
+    products = Product.objects.all()
+    return render(request, 'accounts/products.html', {'products':products})
 
-def customer(request):
-    return render(request, 'accounts/customer.html')
+def customer(request,pk):
+    customer = Customer.objects.get(id=pk)
+    orders = customer.orders_set.all()
+    total_orders = orders.count()
+    context = {'customer': customer, 'total_orders': total_orders,
+               'orders': orders}
+    return render(request, 'accounts/customer.html', context)
