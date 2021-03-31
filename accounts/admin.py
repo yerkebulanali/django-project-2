543a5d7732a5252d7cb2dec002c265d947d35616
Yerkebulan <yerkebulanali@gmail.com>
2021-03-31 10:37:32 +0600

    add: Product, Customer, Order, Tag models

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from accounts.models import *
 
-# Register your models here.
+
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Tag)
+admin.site.register(Orders)
