ff4ef94154d4290b413fbe2c4781aadc6e56ac2c
Yerkebulan <yerkebulanali@gmail.com>
2021-04-01 18:24:36 +0600

    add: User Profile With One To One Relationship

--- accounts/views.py
+++ accounts/views.py
@@ -20,6 +20,9 @@ def registerPage(request):
 
             group = Group.objects.get(name='customer')
             user.groups.add(group)
+            Customer.objects.create(
+                user=user,
+                )
 
             messages.success(request, 'Account was created for ' + username)
             return redirect('login')
@@ -63,8 +66,15 @@ def home(request):
     return render(request, 'accounts/dashboard.html', context)
 
 
+@login_required(login_url='login')
+@allowed_users(allowed_roles=['customer'])
 def userPage(request):
-    context = {}
+    orders = request.user.customer.orders_set.all()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+    context = {'orders': orders, 'total_orders': total_orders,
+               'delivered': delivered, 'pending': pending}
     return render(request, 'accounts/user.html', context)
 
 
