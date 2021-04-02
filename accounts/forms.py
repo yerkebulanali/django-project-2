fd71782a91ae62aa0e797ed069a0fe9c68519706
Yerkebulan <yerkebulanali@gmail.com>
2021-04-02 16:07:59 +0600

    add: Image File Upload to Customer Profile

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,16 @@
 from django.forms import ModelForm
-from .models import Orders
+from .models import Orders, Customer
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
+
+
+class CustomerForm(ModelForm):
+    class Meta:
+        model = Customer
+        fields = '__all__'
+        exclude = ['user']
+
 
 class OrderForm(ModelForm):
     class Meta:
